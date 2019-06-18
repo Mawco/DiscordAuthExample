d2ac457e3f19d574f66139fe0c6a0eae11690905
Marco <37117103+MarcosPizza@users.noreply.github.com>
2019-06-18 14:20:33 +0200

    Fixed major bug

--- server.js
+++ server.js
@@ -4,22 +4,27 @@ var express  = require('express')
   , Strategy = require('passport-discord').Strategy
   , app      = express()
   , path = require('path')
-  , config = require("./config");
-passport.serializeUser((user, done) =>{
+  , config = require('./config');
+
+
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
-passport.deserializeUser((obj, done) =>{
+
+passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
+
 var scopes = ['identify', 'email', 'connections', 'guilds'];
+
 
 passport.use(new Strategy({
     clientID: config.AppID,
-    clientSecret: config.ClientSecret,
-    callbackURL: config.callbackURL,
+    clientSecret: config.clientSecret,
+    callbackURL: config.CallbackURL,
     scope: scopes
-}, (accessToken, refreshToken, profile, done) =>{
+}, function(accessToken, refreshToken, profile, done) {
     process.nextTick(function() {
         return done(null, profile);
     });
@@ -42,24 +47,24 @@ app.get('/callback',
 );
 
 
-app.get('/logout', (req, res) =>{
+app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
-app.get('/info', checkAuth, (req, res) =>{
+app.get('/info', checkAuth, function(req, res) {
     //console.log(req.user)
     res.json(req.user);
 });
 
-app.get('/', (req, res) =>{
+app.get('/', function(req, res) {
     res.render('index', { req: req, res: res });
 });
 
-app.get('/servers', (req, res) =>{
+app.get('/servers', function(req, res) {
     res.render('servers', { req: req, res: res });
 });
 
-app.get('/linkedAccounts', (req, res) =>{
+app.get('/linkedAccounts', function(req, res) {
     res.render('linkedAccounts', {req: req, res: res });
 });
 
@@ -69,7 +74,7 @@ function checkAuth(req, res, next) {
 }
 
 
-app.listen(5000, (err) =>{
+app.listen(5000, function(err) {
     if (err) return console.log(err)
     console.log('Listening at http://localhost:5000/')
 })
